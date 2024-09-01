b1e9a9a9a76aeb19ab2b25f12fd6719b1094aee4
Edoardo De Piccoli <103844232+edoardodepiccoli@users.noreply.github.com>
2024-09-01 15:24:03 +0200

    create basic version of game

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,6 @@
 require_relative 'file_utilities'
+require 'colorize'
+require "json"
 
 class Game
   include FileUtilities
@@ -9,26 +11,82 @@ class Game
     @guesses_left = 7
 
     @guessed_letters = []
-
+    @wrong_guesses = []
+    @finished = false
     play_round
   end
 
   private
 
+  def save_game
+    json_save = JSON.dump({
+      "@valid_words" => @valid_words,
+      "@secret_word" => @secret_word,
+      "@guesses_left" => @guesses_left,
+      "@guessed_letters" => @guessed_letters,
+      "@wrong_guesses" => @wrong_guesses,
+      "@finished" => @finished
+    })
+    timestamp = Time.now
+    File.new("saves/#{timestamp}.json")
+  end
+
   def play_round
-    reset_screen
+    unless @finished
+      reset_screen
+      user_guess = get_user_guess
+      handle_user_guess(user_guess)
+
+      decrease_guesses_left
+      check_for_win
+
+      play_round
+    end
+  end
+
+  def check_for_win
+    if @guessed_letters.include?(@secret_word) || @secret_word.split('').all? { |letter| @guessed_letters.include?(letter) }
+      @finished = true
+      puts('you won!'.colorize(:green))
+    elsif @guesses_left == 0
+      @finished = true
+      puts('you lost :('.colorize(:red))
+    end
+  end
+
+  def handle_user_guess(user_guess)
+    if user_guess == 'exit_game'
+      # serialize then finish game
+    end
+
+    if @secret_word.split('').include?(user_guess) || @secret_word == user_guess
+      @guessed_letters.push(user_guess) unless @guessed_letters.include?(user_guess)
+    else
+      @wrong_guesses.push(user_guess)
+    end
+  end
+
+  def get_user_guess
+    puts('choose a letter or try to guess the entire word')
+    user_guess = gets.chomp.downcase
+    while user_guess == '' || @guessed_letters.include?(user_guess)
+      puts('please try again')
+      user_guess = gets.chomp.downcase
+    end
+    user_guess
   end
 
   def reset_screen
     system('clear')
     puts("guesses left => #{@guesses_left}")
-    puts('choose a letter or try to guess the entire word')
+    puts("wrong guessed words and letters => #{@wrong_guesses}")
     puts
     puts(@secret_word)
     display_secret_word
+    puts
   end
 
-  def lower_guesses_left
+  def decrease_guesses_left
     @guesses_left -= 1
   end
 
